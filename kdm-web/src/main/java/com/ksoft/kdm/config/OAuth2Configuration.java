a18fd5358525cdbe32e7beba0e391f29b812f9ca
张建波 <k963@vip.qq.com>
2017-05-09 17:34:44 +0800

    调整2

---
+++ kdm-web/src/main/java/com/ksoft/kdm/config/OAuth2Configuration.java
@@ -0,0 +1,33 @@
+package com.ksoft.kdm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+
+@Configuration
+public class OAuth2Configuration {
+
+    @Configuration
+    @EnableResourceServer
+    protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+        private static final String SERVER_RESOURCE_ID = "oauth2server";
+
+
+        @Override
+        public void configure(ResourceServerSecurityConfigurer resources) {
+            resources.resourceId(SERVER_RESOURCE_ID);
+        }
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http
+                    .requestMatchers().antMatchers("/articles")
+                    .and()
+                    .authorizeRequests()
+                    .antMatchers("/articles").access("#oauth2.hasScope('read')");
+        }
+    }
+}
