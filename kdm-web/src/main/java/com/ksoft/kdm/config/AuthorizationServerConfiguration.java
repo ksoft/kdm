c85ac7793d297b4826bb7a56c22dfba10c2ed797
张建波 <k963@vip.qq.com>
2017-05-12 12:26:56 +0800

    自定义授权页面，未完待续

--- kdm-web/src/main/java/com/ksoft/kdm/config/AuthorizationServerConfiguration.java
+++ kdm-web/src/main/java/com/ksoft/kdm/config/AuthorizationServerConfiguration.java
@@ -10,6 +10,8 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import org.springframework.security.oauth2.provider.approval.JdbcApprovalStore;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
@@ -33,6 +35,11 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
     @Bean
     public TokenStore tokenStore() {
         return new JdbcTokenStore(dataSource);
+    }
+
+    @Bean
+    public ApprovalStore approvalStore(){
+        return new JdbcApprovalStore(dataSource);
     }
 
     @Autowired
